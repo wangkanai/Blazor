c4c6dc25b1076769653c2060854402ea9ca5b647
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-06-21 09:31:43 +0700

    add core structure

---
+++ src/Core/LinqExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+namespace Wangkanai.Blazor.Core
+{
+    public static class LinqExtensions
+    {
+        public static int FindIndex<T>(this IEnumerable<T> items, Func<T, bool> predicate)
+        {
+            
+        }
+    }
+}
